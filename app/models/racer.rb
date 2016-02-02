6ded7b276d611b00af651821477c0fbe00b05ccb
projectman <obushmelev@gmail.com>
2016-02-02 22:50:28 +0300

    Maid up to  rspec spec/crud_spec.rb -e rq05

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,7 +1,29 @@
 class Racer
 
   include ActiveModel::Model
+  
+  attr_accessor :number, :first_name, :last_name, :gender, :group, :secs, :id
+  
+  # initialize from both a Mongo and Web hash
+  def initialize(params={})
+    #switch between both internal and external views of id and population
+    @id=params[:_id].nil? ? params[:id] : params[:_id].to_s
+    @number=params[:number].to_i
+    @first_name=params[:first_name]
+    @last_name=params[:last_name]
+    @gender=params[:gender]
+    @group = params[:group]
+    @secs = params[:secs].to_i
+  end
 
+  # locate a specific document. Use initialize(hash) on the result to 
+  # get in class instance form
+  def self.find id
+
+    doc = collection.find(:_id=>BSON::ObjectId(id)).first  
+
+    return doc.nil? ? nil : Racer.new(doc)
+  end
 
   # convenience method for access to client in console
   def self.mongo_client
@@ -13,5 +35,30 @@ class Racer
    self.mongo_client['racers']
   end
 
+  def self.all(prototype={}, sort={:number=>1}, skip=0, limit=nil) # why nil ??? 
+    
+    Rails.logger.debug {"getting all racers, prototype=#{prototype}, sort=#{sort}, skip=#{skip}, limit=#{limit}"}
+
+    result=collection.find(prototype)
+          .sort(sort)
+          .skip(skip)
+
+    result=result.limit(limit) if !limit.nil?
+
+    return result
+  end
+  
+  def save
+  	# insert the current state of the Racer instance into the database
+  	# obtain inserted doc _id from the result and addign the to_s value of )id
+    # to instanse attribute @id
+  	res = self.class.collection
+              .insert_one(_id:@id, number:@number, first_name:@first_name, 
+              	last_name:@last_name, gender:@gender, group:@group, secs:@secs)
+    @id = res.inserted_id.to_s
+    puts @id
+    return @id
+
+  end
 
 end
